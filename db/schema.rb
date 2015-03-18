ef1b74f81f3fda4457a9b9ddd7fc31a4bfc5946a
Andre Costa <andreccosta@me.com>
2015-03-18 01:06:34 +0000

    Add file upload. Missing controller action

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318004423) do
+ActiveRecord::Schema.define(version: 20150318010205) do
 
   create_table "data_files", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "task_id"
   end
+
+  add_index "data_files", ["task_id"], name: "index_data_files_on_task_id"
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name",           limit: 255
